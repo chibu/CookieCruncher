d1dff4f9f31066fa2b39009fddee117b686cd1e8
Daniel Lanigan <daniel@chibu.net>
2022-01-23 10:32:32 -0500

    Use autoclick to compute cookies per second

--- CookieCruncher.js
+++ CookieCruncher.js
@@ -231,8 +231,14 @@ var CookieCruncher = {
                    <div class="line"></div>
                    <div id="dealsBuildings">`;
     }
+
+    var cookiesPs = Game.cookiesPs,
+        autoClick = CookieCruncher.clickers.autoClick.settings;
+    if (autoClick.on) {
+      cookiesPs += (autoClick.count * autoClick.perSecond) * Game.computedMouseCps;
+    }
     CookieCruncher.buildings.forEach(building => {
-      var time = (building.price-Game.cookies)/Game.cookiesPs;
+      var time = (building.price-Game.cookies)/cookiesPs;
       var value = Math.round(((building.value/CookieCruncher.buildings[0].value)-1)*100);
       output += `
         <div class="icon" style="background-position:${(-48*building.icon)}px 0px;"></div>
@@ -244,18 +250,18 @@ var CookieCruncher = {
         </div>
         <div class="name">${building.name}</div>
         <small>
-          [owned: ${building.count}]
-          ${(time > 0 ? `${Math.floor(time/86400)}, ${Math.floor(time%2586400/3600)}:${+Math.floor(time%253600/60)}:${Math.floor(time%2560)}`:'')}
+          [owned: ${building.count}]<br>
+          ${Game.sayTime(time*Game.fps, -1)}
         </small>
         <div class="line"></div>
       `
     });
-    output += `<div style="font-weight:bold">per second: <span class="price">${Beautify(Game.cookiesPs)}</span>
-                <br>per minute: <span class="price">${Beautify(Game.cookiesPs*60)}</span>
-                <br>per 10 minutes: <span class="price">${Beautify(Game.cookiesPs*600)}</span>
-                <br>per 30 minutes: <span class="price">${Beautify(Game.cookiesPs*1800)}</span>
-                <br>per hour: <span class="price">${Beautify(Game.cookiesPs*3600)}</span>
-                <br>per day: <span class="price">${Beautify(Game.cookiesPs*86400)}</span>
+    output += `<div style="font-weight:bold">per second: <span class="price">${Beautify(cookiesPs)}</span>
+                <br>per minute: <span class="price">${Beautify(cookiesPs*60)}</span>
+                <br>per 10 minutes: <span class="price">${Beautify(cookiesPs*600)}</span>
+                <br>per 30 minutes: <span class="price">${Beautify(cookiesPs*1800)}</span>
+                <br>per hour: <span class="price">${Beautify(cookiesPs*3600)}</span>
+                <br>per day: <span class="price">${Beautify(cookiesPs*86400)}</span>
                 </div>`
 
     if (!draw) output += `</div></div>`;
