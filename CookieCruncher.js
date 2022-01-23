f26a93351dd9a135ef868ba0a64ccb4d427d291a
Daniel Lanigan <daniel@chibu.net>
2022-01-22 19:52:22 -0500

    Reformat Best Deals

--- CookieCruncher.js
+++ CookieCruncher.js
@@ -211,40 +211,43 @@ var CookieCruncher = {
   			if (!building.locked)
   				CookieCruncher.buildings.push({
   					'icon': building.iconColumn,
-  					'price': Beautify(Math.round(building.price)),
-  					'canBuy':Game.cookies>=building.price,
+  					'price': building.bulkPrice,
+  					'canBuy':Game.cookies>=building.bulkPrice,
   					'name': building.name,
-  					'cost': building.price,
   					'count': building.amount,
-  					'value': building.price/(building.storedCps*Game.globalCpsMult)
-  				})
+  					'value': building.bulkPrice/(building.storedCps*Game.globalCpsMult*Game.buyBulk)
+  				});
+
   		});
   		CookieCruncher.buildings.sort((a, b) => a.value - b.value);
   		output = '<div class="store-tooltip">';
   		CookieCruncher.buildings.forEach(building => {
-  			var time = (building.cost-Game.cookies)/Game.cookiesPs;
-  			output += ''+
-  			'<div class="icon" style="background-position:'+(-48*building.icon)+'px 0px;"></div>'+
-  			'<div class="prices">'+
-  				'<small>+</small>' + Math.round(((building.value/CookieCruncher.buildings[0].value)-1)*100) + '%<br>'+
-  				'<small>'+
-  					'<span class="price '+(building.canBuy?'':'disabled')+'" style="padding-top:3px">'+building.price+'</span>'+
-  				'</small>'+
-  			'</div>'+
-  			'<div class="name">' + building.name + '</div>'+
-  			'<small>[owned: '+building.count+'] '+(time>0?Math.floor(time/86400)+","+
-  				Math.floor(time%2586400/3600)+":"+Math.floor(time%253600/60)+':'+Math.floor(time%2560):'')+
-  			'</small>'+
-  			'<div class="line"></div>'
+  			var time = (building.price-Game.cookies)/Game.cookiesPs;
+        var value = Math.round(((building.value/CookieCruncher.buildings[0].value)-1)*100);
+  			output += `
+    			<div class="icon" style="background-position:${(-48*building.icon)}px 0px;"></div>
+    			<div class="prices">
+    				${(value > 0 ? '<small>+</small>' + Beautify(value) + '%<br>' : '<br>')}
+    				<small>
+    					<span class="price ${(building.canBuy?'':'disabled')}" style="padding-top:3px">${Beautify(Math.round(building.price))}</span>
+    				</small>
+    			</div>
+    			<div class="name">${building.name}</div>
+    			<small>
+            [owned: ${building.count}]
+            ${(time > 0 ? `${Math.floor(time/86400)}, ${Math.floor(time%2586400/3600)}:${+Math.floor(time%253600/60)}:${Math.floor(time%2560)}`:'')}
+    			</small>
+    			<div class="line"></div>
+        `
   		});
-  		output += '<div style="font-weight:bold">per second: <span class="price">'+Beautify(Game.cookiesPs)+'</span>'+
-                  '<br>per minute: <span class="price">'+Beautify(Game.cookiesPs*60)+'</span>'+
-                  '<br>per 10 minutes: <span class="price">'+Beautify(Game.cookiesPs*600)+'</span>'+
-                  '<br>per 30 minutes: <span class="price">'+Beautify(Game.cookiesPs*1800)+'</span>'+
-                  '<br>per hour: <span class="price">'+Beautify(Game.cookiesPs*3600)+'</span>'+
-                  '<br>per day: <span class="price">'+Beautify(Game.cookiesPs*86400)+'</span>'+
-                  '</div>'+
-              '</div>';
+  		output += `<div style="font-weight:bold">per second: <span class="price">${Beautify(Game.cookiesPs)}</span>
+                  <br>per minute: <span class="price">${Beautify(Game.cookiesPs*60)}</span>
+                  <br>per 10 minutes: <span class="price">${Beautify(Game.cookiesPs*600)}</span>
+                  <br>per 30 minutes: <span class="price">${Beautify(Game.cookiesPs*1800)}</span>
+                  <br>per hour: <span class="price">${Beautify(Game.cookiesPs*3600)}</span>
+                  <br>per day: <span class="price">${Beautify(Game.cookiesPs*86400)}</span>
+                  </div>
+              </div>`;
   		return output;
   	},'store');
   	Game.tooltip.wobble();
